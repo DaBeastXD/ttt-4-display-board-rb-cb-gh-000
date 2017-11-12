006abedb5873fbcc435ce173a456af804950b1a2
Kevin Le <17786629@student.westernsydney.edu.au>
2017-11-12 05:27:20 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+board = [" "," "," "," "," "," "," "," "," "]
+display_board(board)
+def display_board 
 end
